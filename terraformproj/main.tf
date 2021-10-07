0561179ac09faaf2e6e839259960b3ac8a16b805
AndrewReid-Journey <91478458+AndrewReid-Journey@users.noreply.github.com>
2021-10-07 12:07:02 +0100

    remove terraform block

--- terraformproj/main.tf
+++ terraformproj/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "reidacus-terraformdev"
-
-    workspaces {
-      name = "terraformdev"
-    }
-  }
-}
-
 provider "aws" {
     region = "eu-west-1"
 }
