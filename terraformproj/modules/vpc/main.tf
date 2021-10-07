f385f3d2c9e29135655659882a50751d4ed31032
AndrewReid-Journey <91478458+AndrewReid-Journey@users.noreply.github.com>
2021-10-07 18:54:44 +0100

    add vpc module

---
+++ terraformproj/modules/vpc/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">=0.12"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "mainvpc"
+  }
+}
---
+++ terraformproj/vpc.tf
@@ -0,0 +1,3 @@
+module "newvpc" {
+  source = "./modules/vpc"
+}
