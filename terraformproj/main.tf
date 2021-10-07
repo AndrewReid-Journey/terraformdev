08ebe09b32edf856120123538b44be4cf3e15b5a
AndrewReid-Journey <91478458+AndrewReid-Journey@users.noreply.github.com>
2021-10-07 18:18:57 +0100

    Add terraform cloud declaration

--- terraformproj/main.tf
+++ terraformproj/main.tf
@@ -1,3 +1,20 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "reidacus"
+
+    workspaces {
+      name = "terraformdev"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.55.0"
+    }
+  }
+}
+
 provider "aws" {
     region = "eu-west-1"
 }
