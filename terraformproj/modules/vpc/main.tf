84e24daf15b31cdc12f79abecbf2638bdd44825b
AndrewReid-Journey <91478458+AndrewReid-Journey@users.noreply.github.com>
2021-10-08 10:29:36 +0100

    another test

--- terraformproj/modules/vpc/main.tf
+++ terraformproj/modules/vpc/main.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "myvpc" {
     tags = {
-      "name" = var.vpcname
+      "Name" = var.vpcname
     }
     
-    cidr_block = "10.0.0.0/24"
+    cidr_block = "10.0.2.0/24"
   
 }
